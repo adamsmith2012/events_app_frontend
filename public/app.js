1991032d2ccf4eaed3ea3ef1800fcaf321b6871e
588870 <adamsmith@c1mrq060hf4h.attlocal.net>
2017-05-22 12:56:49 -0700

    added redirect on page reload

--- public/app.js
+++ public/app.js
@@ -170,7 +170,6 @@ app.controller('mainController', ['$http', '$location', function($http, $locatio
 }]);
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) { //.config just runs once on load
-    $locationProvider.html5Mode({ enabled: true, requireBase: false }); // tell angular to use push state
     $routeProvider
     .when("/", {
         templateUrl : "/partials/index-partial.html"
@@ -187,4 +186,5 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
     .when("/event/edit", {
         templateUrl : "/partials/edit-event.html"
     });
+    $locationProvider.html5Mode({ enabled: true, requireBase: false }); // tell angular to use push state
 }]);
